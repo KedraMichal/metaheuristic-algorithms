5a7970a89405a00ec5b8aca24f7990be1503fb30
ked-m <ked-mic@wp.pl>
2020-01-22 21:47:39 +0100

    tabu search

--- tabu.py
+++ tabu.py
@@ -29,27 +29,25 @@ def odch(data):
 
 tabu_list =[]
 def tabu(tabu_lis, tabu1, tabu2):
-    tabu_lis = tabu_list
     tabu_lis.append((tabu1, tabu2))
     if len(tabu_lis)>3:
         del tabu_lis[0]
 
 def main(data):
     comb = []
-    print("dzial")
     add_first = True
     minimalna = 0
     print(tabu_list)
     for i in range(count_rows-1):
         for j in range(i+1, count_rows):
-            x1 = df.iloc[i, 0] -1
+            x1 = df.iloc[i, 0] -1# po nr zadania, zeby potem dobrze byly w tabu dodane
             x2 = df.iloc[j, 0] -1
             swap(data,x1, x2)
             whenend(data)
             odch(data)
             odchylenia = data.odchy.sum()
             comb.append(odchylenia)
-            if (x1, x2) not in tabu_list or (x2, x1) not in tabu_list:
+            if (x1, x2) not in tabu_list and (x2, x1) not in tabu_list:
                 if add_first is True or odchylenia < minimalna:
                     add_first = False
                     first = x1
@@ -74,8 +72,6 @@ for i in range(15):
     df = final.copy()
 
 print(final.odchy.sum())
-
-
 final.to_csv("tabu_result.csv", index=False)
 
 
